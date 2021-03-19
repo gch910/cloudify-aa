9acdb3a63c3e7b95d5dcb0678a1019d7ec2405d7
joshua <joshuagmiller2289@gmail.com>
2021-03-19 14:48:59 -0700

    updated models

--- app/models/song.py
+++ app/models/song.py
@@ -7,6 +7,8 @@ class Song(db.Model):
   title = db.Column(db.String(1000), nullable=False )
   release_date = db.Column(db.Date, nullable=False)
   song_path = db.Column(db.String(1000), nullable=False)
-  image_path = db.Colunm(db.String(1000), nullable=False)
-  user_id = db.Column(db.Integer, nullable=False)
-  genre_id = db.Column(db.Integer, nullable=False)
+  image_path = db.Colunm(db.String(1000), default='https://edm.com/.image/t_share/MTU5NDY5Nzk2NTUzOTI1OTA1/soundcloud.png' nullable=False)
+  user_id = db.Column(db.Integer, nullable=False, ForeignKey='users.id')
+  
+  genre_id = db.Column(db.Integer, nullable=False, ForeignKey='genres.id')
+  genre = db.relationship('Genre')
