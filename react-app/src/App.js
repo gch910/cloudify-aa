1baca58dfb78332bc60bce3f2942ab796b51c2b3
Gabriel Harris <gch910@gmail.com>
2021-03-22 13:13:16 -0500

    creating navbar layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
@@ -7,20 +8,23 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import Home from "./components/Home";
-import { authenticate } from "./services/auth";
+import Navigation from "./components/Navigation";
+import { restoreUser } from "./store/session";
+
 
 function App() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.user);
   const [authenticated, setAuthenticated] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const user = await authenticate();
+      const user = await dispatch(restoreUser())
       if (!user.errors) {
         setAuthenticated(true);
       }
       setLoaded(true);
-      return user;
     })();
   }, []);
 
@@ -28,9 +32,17 @@ function App() {
     return null;
   }
 
+  let navId;
+
+  if (sessionUser) {
+    navId = "logged-in-nav";
+  } else {
+    navId = "logged-out-nav";
+  }
+
   return (
     <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
+      <Navigation setAuthenticated={setAuthenticated} />
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm
---
+++ react-app/src/components/Navigation/index.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { restoreUser } from "../../store/session";
+import LogoutButton from "../auth/LogoutButton"
+
+const Navigation = ({setAuthenticated}) => {
+  const sessionUser = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(restoreUser()).then((req) => setIsLoaded(true));
+  }, [dispatch]);
+
+  let sessionLinks;
+
+  if (sessionUser) {
+    sessionLinks = (
+      <>
+        <NavLink className="nav-link" id="home-link" exact to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className="nav-link"
+          id="music-link"
+          exact
+          to={`/${sessionUser.id}/music`}
+        >
+          Music
+        </NavLink>
+        <h1>SearchBar</h1>
+        <NavLink
+          className="nav-link"
+          id="upload-link"
+          exact
+          to={`/${sessionUser.id}/upload`}
+        >
+          Upload
+        </NavLink>
+        {/* <NavLink>Profile</NavLink> */}
+        <LogoutButton setAuthenticated={setAuthenticated} />
+      </>
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink className="nav-link" id="home-link" exact to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className="nav-link"
+          id="music-link"
+          to={`/${sessionUser.id}/music`}
+        >
+          Music
+        </NavLink>
+        <h1>SearchBar</h1>
+        <NavLink
+          className="nav-link"
+          id="upload-link"
+          to={`/${sessionUser.id}/upload`}
+        >
+          Upload
+        </NavLink>
+        <h1>Sign In</h1>
+        <h1>Create Account</h1>
+      </>
+    );
+  }
+  return (
+    <div id="nav-container">
+      <nav>{isLoaded && sessionLinks}</nav>
+    </div>
+  );
+};
+
+export default Navigation;
