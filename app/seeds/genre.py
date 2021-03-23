5fa3c6f3df96ad1042edee9b7049912298083f7a
joshua <joshuagmiller2289@gmail.com>
2021-03-23 16:27:04 -0700

    updates song form info

--- app/seeds/genre.py
+++ app/seeds/genre.py
@@ -2,14 +2,12 @@ from app.models import db, Genre
 
 def seed_genres():
 
-  genre1 = Genre(name='Rap')
-  genre2 = Genre(name='Rock')
-  genre3 = Genre(name='R&B')
-  genre4 = Genre(name='Pop')
-  genre5 = Genre(name='Soul')
-  genre6 = Genre(name='Electronic')
-  genre7 = Genre(name='Country')
-  genre8 = Genre(name='Jazz')
+  genre1 = Genre(name='Rap')##Josh
+  genre2 = Genre(name='Rock') ##Gabriel
+  genre3 = Genre(name='R&B')##Jevon
+  genre4 = Genre(name='Pop')##Josh
+  genre5 = Genre(name='Jazz') ##Gabriel
+  genre6 = Genre(name='Electronic') ##Jevon
 
   db.session.add(genre1)
   db.session.add(genre2)
@@ -17,6 +15,4 @@ def seed_genres():
   db.session.add(genre4)
   db.session.add(genre5)
   db.session.add(genre6)
-  db.session.add(genre7)
-  db.session.add(genre8)
   db.session.commit()
--- react-app/src/components/UploadSongForm/index.js
+++ react-app/src/components/UploadSongForm/index.js
@@ -5,41 +5,75 @@ const UploadSongForm = () => {
   const history = useHistory();
   const [song, setSong] = useState(null);
   const [songLoading, setSongLoading] = useState(false);
+  const [image, setImage] = useState(null);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState(Date());
+  // const [image, setImage] = useState(null);
+  // const [imageLoading, setImageLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("song", song);
 
-    // aws uploads can be a bit slow—displaying
-    // some sort of loading message is a good idea
-    setSongLoading(true);
+    // // aws uploads can be a bit slow—displaying
+    // // some sort of loading message is a good idea
+    // setSongLoading(true);
 
-    const res = await fetch("/api/songs/upload", {
-      method: "POST",
-      body: formData,
-    });
-    if (res.ok) {
-      await res.json();
-      setSongLoading(false);
-      history.push("/");
-    } else {
-      setSongLoading(false);
-      // a real app would probably use more advanced
-      // error handling
-      console.log("error");
-    }
+    // const res = await fetch("/api/songs/upload", {
+    //   method: "POST",
+    //   body: formData,
+    // });
+    // if (res.ok) {
+    //   await res.json();
+    //   setSongLoading(false);
+    //   history.push("/");
+    // } else {
+    //   setSongLoading(false);
+    //   // a real app would probably use more advanced
+    //   // error handling
+    //   console.log("error");
+    // }
   };
 
-  const updateImage = (e) => {
+  const updateSong = (e) => {
     const file = e.target.files[0];
     setSong(file);
   };
+  const updateImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <input type="file" accept="audio/*" onChange={updateImage} />
+      <div>
+        <label>Song Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Release Date</label>
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Upload a song file</label>
+        <input type="file" accept="image/*" onChange={updateSong} />
+        {songLoading && <p>Loading...</p>}
+      </div>
+      <div>
+        <label>Upload an album image</label>
+        <input type="file" accept="audio/*" onChange={updateImage} />
+        {songLoading && <p>Loading...</p>}
+      </div>
       <button type="submit">Submit</button>
-      {songLoading && <p>Loading...</p>}
     </form>
   );
 };
