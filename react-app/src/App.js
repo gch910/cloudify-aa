39b5f944c0d2f57c8a4f136916949c10a571aa94
Hero <heroroku@gmail.com>
2021-03-23 14:14:07 -0400

    working on playbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import Home from "./components/Home";
+import PlayBar from "./components/PlayBar";
 import { authenticate } from "./services/auth";
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar setAuthenticated={setAuthenticated} />
+      <PlayBar />
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm
