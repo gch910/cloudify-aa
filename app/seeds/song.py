a9bc5da9720c759d2fd3b7aa3f1ea0b842c9ed49
Gabriel Harris <gch910@gmail.com>
2021-03-24 11:11:18 -0500

    rock and jazz seed data

--- app/seeds/song.py
+++ app/seeds/song.py
@@ -36,48 +36,7 @@ def seed_songs():
   #   genre_id = 2,
   # )
 
-  gabe1 = Song(
-    title='Hold On',
-    release_date = date(2012, 2, 6),
-    song_path = "https://www.youtube.com/watch?v=Le-3MIBxQTw",
-    image_path = 'https://cdn2.thelineofbestfit.com/images/remote/http_cdn2.thelineofbestfit.com/media/2012/04/alabama-shakes-boys-and-girls.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
-  gabe2 = Song(
-    title='Sound and Color',
-    release_date = date(2015, 4, 17),
-    song_path = "https://www.youtube.com/watch?v=faG8RiaANek",
-    image_path = 'https://consequenceofsound.net/wp-content/uploads/2015/04/alabama-shakes-sound-and-color.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
-  gabe3 = Song(
-    title='Hang Loose',
-    release_date = date(2012, 4, 9),
-    song_path = "https://www.youtube.com/watch?v=4sSl11GMkMI",
-    image_path = 'https://cdn2.thelineofbestfit.com/images/remote/http_cdn2.thelineofbestfit.com/media/2012/04/alabama-shakes-boys-and-girls.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
-
-  gabe4 = Song(
-    title='15 Step',
-    release_date = date(2007, 10, 10),
-    song_path = "https://www.youtube.com/watch?v=bgsmr7t8zGE",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81KWYrlOt7L._SL1254_.jpg',
-    user_id = "Radiohead", 
-    genre_id = 2,
-  )
   
-  gabe4 = Song(
-    title='15 Step',
-    release_date = date(2007, 10, 10),
-    song_path = "https://www.youtube.com/watch?v=bgsmr7t8zGE",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81KWYrlOt7L._SL1254_.jpg',
-    user_id = "Radiohead", 
-    genre_id = 2,
-  )
 
   
 
