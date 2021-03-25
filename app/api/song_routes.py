47d46a0939827d0bc0952539f7e66def5e19812b
joshua <joshuagmiller2289@gmail.com>
2021-03-25 01:04:57 -0700

    pushing image and song to aws working

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Song, Genre
-from app.aws import upload_file_to_s3, allowed_audio_file, get_unique_filename
+from app.aws import allowed_image_file, upload_file_to_s3, allowed_audio_file, get_unique_filename
 
 song_routes = Blueprint('songs', __name__)
+
 
 @song_routes.route("/")
 def songs():
@@ -11,42 +12,63 @@ def songs():
     songsDict = {"songs": [song.to_dict() for song in songs]}
     return songsDict
 
+
 @song_routes.route('/upload', methods=['POST'])
 def song_upload():
     print(request.files['song'])
-    # print(request.files['image'])
-    # defaultImage = False
-    # if 'song' not in request.files:
-    #     return {'errors': 'song required'}, 400
-    # if 'image' not in request.files:
-    #     defaultImage= = True
+    defaultImage = False
+    if 'song' not in request.files:
+        return {'errors': 'song required'}, 400
+    if 'image' not in request.files:
+        defaultImage = True
     # print(defaultImage)
-    
-    # song = request.files['song']
-    # image = request.files['image']
-    # if image == '':
-    #     defaultImage=True
-    
-    
-    # if not allowed_audio_file(song.filename):
-    #     return {'errors': 'file type not permitted'}, 400
 
-    # song.filename = get_unique_filename(song.filename)
+    song = request.files['song']
+    image = ''
+    if defaultImage:
+        image = defaultImage
+    else:
+        image = request.files['image']
 
-    # song_upload = upload_file_to_s3(song)
+    if not allowed_audio_file(song.filename):
+        return {'errors': 'file type not permitted'}, 400
 
-    # if "url" not in song_upload:
-    #     ##if the dictionary doesn't have a url key
-    #     ##it means that there was an error when we tried to song_upload
-    #     ##so we send back that error message
-    #     return song_upload, 400
+    song.filename = get_unique_filename(song.filename)
 
-    # song_url = song_upload['url']
-    # print(song_url)
-    # newSong = Song(
-    #     title = 
-    # )
-    return {'ok':'hello'}, 200
+    song_upload = upload_file_to_s3(song)
+
+    if "url" not in song_upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to song_upload
+        # so we send back that error message
+        return song_upload, 400
+
+    song_url = song_upload['url']
+    image_url = ''
+    if not defaultImage:
+        if not allowed_image_file(image.filename):
+            return {'errors': 'file type not permitted'}, 400
+
+        image.filename = get_unique_filename(image.filename)
+
+        image_upload = upload_file_to_s3(image)
+
+        if "url" not in image_upload:
+            # if the dictionary doesn't have a url key
+            # it means that there was an error when we tried to song_upload
+            # so we send back that error message
+            return image_upload, 400
+        image_url = image_upload['url']
+
+    else:
+        image_url = 'https://cloudify.s3-us-west-2.amazonaws.com/3e29a4568cc911eb8dcd0242ac130003.png'
+
+    print(song_url)
+    print(image_url)
+    newSong = Song(
+        title=)
+    return {'ok': 'hello'}, 200
+
 
 @song_routes.route('/genres')
 def song_genre():
--- app/aws.py
+++ app/aws.py
@@ -6,12 +6,12 @@ import uuid
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
-ALLOWED_AUDIO_EXTENSIONS = {"wav", 'mp3', 'ogg' }
+ALLOWED_AUDIO_EXTENSIONS = {"wav", 'mp3', 'ogg'}
 
 s3 = boto3.client(
-   "s3",
-   aws_access_key_id=os.environ.get("S3_KEY"),
-   aws_secret_access_key=os.environ.get("S3_SECRET")
+    "s3",
+    aws_access_key_id=os.environ.get("S3_KEY"),
+    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
 
 
@@ -19,9 +19,10 @@ def allowed_audio_file(filename):
     return "." in filename and \
            filename.rsplit(".", 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
 
+
 def allowed_image_file(filename):
     return "." in filename and \
-           filename.rsplit(".", 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def get_unique_filename(filename):
