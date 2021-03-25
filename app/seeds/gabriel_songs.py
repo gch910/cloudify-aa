6e5c06cd7ef38f92d0c086c1737123ab32937161
joshua <joshuagmiller2289@gmail.com>
2021-03-25 11:00:35 -0700

    gabriels seed data updated

--- app/seeds/gabriel_songs.py
+++ app/seeds/gabriel_songs.py
@@ -1,147 +1,147 @@
 rock1 = Song(
     title='Hold On',
-    release_date = date(2012, 2, 6),
-    song_path = "https://www.youtube.com/watch?v=Le-3MIBxQTw",
-    image_path = 'https://cdn2.thelineofbestfit.com/images/remote/http_cdn2.thelineofbestfit.com/media/2012/04/alabama-shakes-boys-and-girls.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
-  rock2 = Song(
+    release_date=date(2012, 2, 6),
+    song_path="https://cloudify.s3.amazonaws.com/8d01684af6984097bff7a83e832e8fe4.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/4ac7d4e806974bbba89beebe94cd5305.jpg',
+    user_id=1,
+    genre_id=2,
+)
+rock2 = Song(
     title='Sound and Color',
-    release_date = date(2015, 4, 17),
-    song_path = "https://www.youtube.com/watch?v=faG8RiaANek",
-    image_path = 'https://consequenceofsound.net/wp-content/uploads/2015/04/alabama-shakes-sound-and-color.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
-  rock3 = Song(
+    release_date=date(2015, 4, 17),
+    song_path="https://cloudify.s3.amazonaws.com/4f77ee73c02949f6bf1ca79d4bc35e70.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/c3a4616b02e345b7a4a316df7ed41db4.jpg',
+    user_id=1,
+    genre_id=2,
+)
+rock3 = Song(
     title='Hang Loose',
-    release_date = date(2012, 4, 9),
-    song_path = "https://www.youtube.com/watch?v=4sSl11GMkMI",
-    image_path = 'https://cdn2.thelineofbestfit.com/images/remote/http_cdn2.thelineofbestfit.com/media/2012/04/alabama-shakes-boys-and-girls.jpg',
-    user_id = "Alabama Shakes", 
-    genre_id = 2,
-  )
+    release_date=date(2012, 4, 9),
+    song_path="https://cloudify.s3.amazonaws.com/e5d4a8bb418f463ab5bf4e94c72efa69.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/13eac44b2d224c06a60fb27cca5fc53e.jpg',
+    user_id=1,
+    genre_id=2,
+)
 
-  rock4 = Song(
+rock4 = Song(
     title='15 Step',
-    release_date = date(2007, 10, 10),
-    song_path = "https://www.youtube.com/watch?v=bgsmr7t8zGE",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81KWYrlOt7L._SL1254_.jpg',
-    user_id = "Radiohead", 
-    genre_id = 2,
-  )
-  
-  rock5 = Song(
+    release_date=date(2007, 10, 10),
+    song_path="https://cloudify.s3.amazonaws.com/82b5dd6baef549c6a3a1648ec487924a.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/13eac44b2d224c06a60fb27cca5fc53e.jpg',
+    user_id=2,
+    genre_id=2,
+)
+
+rock5 = Song(
     title='Bodysnatchers',
-    release_date = date(2007, 10, 10),
-    song_path = "https://www.youtube.com/watch?v=nIQxhSvSRnQ",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81KWYrlOt7L._SL1254_.jpg',
-    user_id = "Radiohead", 
-    genre_id = 2,
-  )
-  rock6 = Song(
+    release_date=date(2007, 10, 10),
+    song_path="https://cloudify.s3.amazonaws.com/b9b64ab380fe456fa8400d674186c2a0.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/13eac44b2d224c06a60fb27cca5fc53e.jpg',
+    user_id=2,
+    genre_id=2,
+)
+rock6 = Song(
     title='Paranoid Android',
-    release_date = date(1997, 5, 21),
-    song_path = "https://www.youtube.com/watch?v=Lt8AfIeJOxw",
-    image_path = 'https://www.thisdayinmusic.com/wp-content/uploads/2018/05/ok-computer.jpg',
-    user_id = "Radiohead", 
-    genre_id = 2,
-  )
-  rock7 = Song(
+    release_date=date(1997, 5, 21),
+    song_path="https://cloudify.s3.amazonaws.com/bab219c23c7b4ccf90614d87e7e154da.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/40f12681daa54ed8a9ec769d90ef1bed.jpg',
+    user_id=2,
+    genre_id=2,
+)
+rock7 = Song(
     title='When My Train Pulls In',
-    release_date = date(2012, 10, 22),
-    song_path = "https://www.youtube.com/watch?v=gFndWoAAi1k",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81kgsZqY1rL._SX522_.jpg',
-    user_id = "Gary Clark Jr.", 
-    genre_id = 2,
-  )
-  rock8 = Song(
+    release_date=date(2012, 10, 22),
+    song_path="https://cloudify.s3.amazonaws.com/9ef06cf5c9234a11adc355eb3f13f60e.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/7c05b9a754454bc7ad5bd9584c081916.jpg',
+    user_id=3,
+    genre_id=2,
+)
+rock8 = Song(
     title='Bright Lights',
-    release_date = date(2012, 10, 22),
-    song_path = "https://www.youtube.com/watch?v=CFjMeOnqAPI",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81kgsZqY1rL._SX522_.jpg',
-    user_id = "Gary Clark Jr.", 
-    genre_id = 2,
-  )
-  rock9 = Song(
+    release_date=date(2012, 10, 22),
+    song_path="https://cloudify.s3.amazonaws.com/38a273c714cb4352802826e3e3bcb52e.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/7c05b9a754454bc7ad5bd9584c081916.jpg'',
+    user_id=3,
+    genre_id=2,
+)
+rock9 = Song(
     title='What About Us',
-    release_date = date(2019, 2, 22),
-    song_path = "https://www.youtube.com/watch?v=7UXVx-4BCMc",
-    image_path = 'https://i.redd.it/6jvcsol05co21.jpg',
-    user_id = "Gary Clark Jr.", 
-    genre_id = 2,
-  )
-  
-  jazz1 = Song(
-    title='Kind of Blue',
-    release_date = date(1959, 8, 17),
-    song_path = "https://www.youtube.com/watch?v=3hpJs6JRL2w",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81CP1j-zprL._SL1500_.jpg',
-    user_id = "Miles Davis", 
-    genre_id = 6,
-  )
-  jazz2 = Song(
+    release_date=date(2019, 2, 22),
+    song_path="https://cloudify.s3.amazonaws.com/a7d200bab06f4e79ac41920b4b56d36c.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/aaa66b9a894243a09905e1c3f936045e.jpg',
+    user_id=3,
+    genre_id=2,
+)
+
+jazz1 = Song(
+    title='Blue In Green',
+    release_date=date(1959, 8, 17),
+    song_path="https://cloudify.s3.amazonaws.com/271d29595c3a449c92cf1b988eb873a5.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/b3bff695d80148ae99c293ffafd31029.jpg',
+    user_id=4,
+    genre_id=6,
+)
+jazz2 = Song(
     title='So What',
-    release_date = date(1959, 8, 17),
-    song_path = "https://www.youtube.com/watch?v=ylXk1LBvIqU,
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81CP1j-zprL._SL1500_.jpg',
-    user_id = "Miles Davis", 
-    genre_id = 6,
-  )
-  jazz3 = Song(
+    release_date=date(1959, 8, 17),
+    song_path="https://cloudify.s3.amazonaws.com/271779e4a5ea47818dfb23846f9d4dc0.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/b3bff695d80148ae99c293ffafd31029.jpg',
+    user_id=4,
+    genre_id=6,
+)
+jazz3 = Song(
     title='All Blues',
-    release_date = date(1959, 8, 17),
-    song_path = "https://www.youtube.com/watch?v=-488UORrfJ0",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/81CP1j-zprL._SL1500_.jpg',
-    user_id = "Miles Davis", 
-    genre_id = 6,
-  )
-  jazz4 = Song(
+    release_date=date(1959, 8, 17),
+    song_path="https://cloudify.s3.amazonaws.com/36aeedca40564bbe84352c34401ac597.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/b3bff695d80148ae99c293ffafd31029.jpg',
+    user_id=4,
+    genre_id=6,
+)
+jazz4 = Song(
     title="Monk's Dream",
-    release_date = date(1963, 3, 10),
-    song_path = "https://www.youtube.com/watch?v=-iQFf2_z0qE",
-    image_path = 'https://img.discogs.com/JKsQ-QfOj7voTtXifAo0Nc6EXoo=/fit-in/600x603/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-735430-1362025845-1626.jpeg.jpg',
-    user_id = "Thelonious Monk", 
-    genre_id = 6,
-  )
-  jazz5 = Song(
+    release_date=date(1963, 3, 10),
+    song_path="https://cloudify.s3.amazonaws.com/3fac34b8e5b04b7ab36ac3e29f4497f7.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/57f701d9e5564c929c1011a63080e6c3.jpg',
+    user_id=5,
+    genre_id=6,
+)
+jazz5 = Song(
     title="Evidence",
-    release_date = date(1948, 7, 2),
-    song_path = "https://www.youtube.com/watch?v=bZQ8zds7mmg",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/71Rfvs1PTjL._SL1200_.jpg',
-    user_id = "Thelonious Monk", 
-    genre_id = 6,
-  )
-  jazz6 = Song(
+    release_date=date(1948, 7, 2),
+    song_path="https://cloudify.s3.amazonaws.com/addf616878e64c028b50b425fd0c9b88.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/d27d54be76ed492eab494c784b75fadb.jpg',
+    user_id=5,
+    genre_id=6,
+)
+jazz6 = Song(
     title="Well You Needn't",
-    release_date = date(1957, 11, 1),
-    song_path = "https://www.youtube.com/watch?v=FOvKLvWuZjg",
-    image_path = 'https://media.pitchfork.com/photos/5929c1cd5e6ef9596932376a/1:1/w_600/29381403.jpg',
-    user_id = "Thelonious Monk", 
-    genre_id = 6,
-  )
-  jazz7 = Song(
+    release_date=date(1957, 11, 1),
+    song_path="https://cloudify.s3.amazonaws.com/10a8b9f08b2b477fb0b88f3826c935e6.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/d27d54be76ed492eab494c784b75fadb.jpg',
+    user_id=5,
+    genre_id=6,
+)
+jazz7 = Song(
     title="I'll Take Les",
-    release_date = date(1994, 8, 9),
-    song_path = "https://www.youtube.com/watch?v=VLMS8DJGvRA",
-    image_path = 'https://img.discogs.com/_JDtqSXFCSiBBoymGIkV6mSz-WI=/fit-in/600x592/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1309399-1340996022-8078.jpeg.jpg',
-    user_id = "John Scofield", 
-    genre_id = 6,
-  )
-  jazz8 = Song(
+    release_date=date(1994, 8, 9),
+    song_path="https://cloudify.s3.amazonaws.com/d0e6f611b9f84c5ea683c74f1aba8c15.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/dc38d774547d49e2b939fa557678fedf.jpg',
+    user_id=6,
+    genre_id=6,
+)
+jazz8 = Song(
     title="A Go Go",
-    release_date = date(1997, 1, 1),
-    song_path = "https://www.youtube.com/watch?v=-PXOr681_VU",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/71W5GfQ9PYL._SL1400_.jpg',
-    user_id = "John Scofield", 
-    genre_id = 6,
-  )
-  jazz9 = Song(
+    release_date=date(1997, 1, 1),
+    song_path="https://cloudify.s3.amazonaws.com/c44d2d38b4194d21ae1af1ac27dc2aa9.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/85053a7fa5964709860cbad265ef3ada.jpg',
+    user_id=6,
+    genre_id=6,
+)
+jazz9 = Song(
     title="Camelus",
-    release_date = date(2013, 7, 2),
-    song_path = "https://www.youtube.com/watch?v=INi6kaG9CT4",
-    image_path = 'https://images-na.ssl-images-amazon.com/images/I/71KD5KbVHvL._SL1400_.jpg',
-    user_id = "John Scofield", 
-    genre_id = 6,
-  )
+    release_date=date(2013, 7, 2),
+    song_path="https://cloudify.s3.amazonaws.com/41a6ae8ab06246e79a093bd7d843995c.mp3",
+    image_path='https://cloudify.s3.amazonaws.com/01d40ae38bb34567871f773118447e96.jpg',
+    user_id=6,
+    genre_id=6,
+)
