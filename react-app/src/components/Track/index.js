2beeca93756468dd375ef815e9f3cb1a7df2cba5
joshua <joshuagmiller2289@gmail.com>
2021-04-27 14:18:18 -0700

    changed a tags to navlinks so song playing persists between pages

--- react-app/src/components/Track/index.js
+++ react-app/src/components/Track/index.js
@@ -1,28 +1,27 @@
 import React from "react";
 import PlayButton from "../PlayButton";
+import { NavLink } from "react-router-dom";
 import "./index.css";
 
-const Track = ({ source, hyperlink, title, artist, user_id }) => {
+const Track = ({ source, hyperlink, title, artist, user_id, song_id }) => {
   return (
-    <a href={hyperlink}>
-      <div className="outer-div">
-        <a href={user_id}>
-          <div>
-            <div className="playbutton"></div>
-            <img alt="Album cover" src={source} />
-            <PlayButton url={hyperlink} />
-          </div>
-        </a>
-        <a href={user_id}>
-          <div>
-            <span className="">{title}</span>
-          </div>
-          <div>
-            <span className="subtitle">{artist}</span>
-          </div>
-        </a>
-      </div>
-    </a>
+    <div className="outer-div">
+      <NavLink to={`/song/${song_id}`}>
+        <div>
+          <div className="playbutton"></div>
+          <img alt="Album cover" src={source} />
+          <PlayButton url={hyperlink} />
+        </div>
+      </NavLink>
+      <NavLink to={`/profile/${user_id}`}>
+        <div>
+          <span className="">{title}</span>
+        </div>
+        <div>
+          <span className="subtitle">{artist}</span>
+        </div>
+      </NavLink>
+    </div>
   );
 };
 
